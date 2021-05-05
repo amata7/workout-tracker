959274aa97d5522fbf1d2decb09bdd38768902af
amata7 <adrianmata@live.com>
2021-05-05 15:09:40 -0700

    connecting mongoose to workout-tracker db

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-// initial server.js serves files in public
-
-// add db connection using mongoose
-
 const express = require('express');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker', {
+  useNewUrlParser: true,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
