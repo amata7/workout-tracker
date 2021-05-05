214c0d513f5dcd7cbc9d3d3a83cb60400d2a8b67
amata7 <adrianmata@live.com>
2021-05-05 15:04:05 -0700

    initial server file

---
+++ server.js
@@ -0,0 +1,17 @@
+// initial server.js serves files in public
+
+// add db connection using mongoose
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
