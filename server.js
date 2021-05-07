016581d21ae790a99df4304b4f5676a880c95c0a
amata7 <adrianmata@live.com>
2021-05-06 17:35:21 -0700

    created first db using workout

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const db = require('./models/Workout');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-  useNewUrlParser: true,
-});
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+});
+
+db.Workout.create({ name: 'Workouts' })
+.then((workouts) => {
+  console.log(workouts);
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
