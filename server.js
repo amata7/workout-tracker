0b02e16cb1f5dfc37c71b80a60ed7d4383d19969
amata7 <adrianmata@live.com>
2021-05-07 14:49:37 -0700

    deploying website using Mongo Atlas + Heroku

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
-const db = require('./models/Workout');
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const db = require("./models/Workout");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutdb', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
-db.Workout.create({ name: 'Workout' })
-.then((dbWorkouts) => {
+db.Workout.create({ name: "Workout" }).then((dbWorkouts) => {
   console.log(dbWorkouts);
 });
 
-app.get('/stats', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/public/stats.html`));
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/public/stats.html`));
 });
 
-app.get('/exercise', (req, res) => {
+app.get("/exercise", (req, res) => {
   res.sendFile(path.join(`${__dirname}/public/exercise.html`));
 });
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
-  
+  console.log(`App running on port ${PORT}!`);
+});
