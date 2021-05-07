b961729217ac7d793cbbc36922d2b12d3d845564
amata7 <adrianmata@live.com>
2021-05-07 15:32:52 -0700

    controllers folder added with routes being exported toward server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
 const db = require("./models/Workout");
+const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,6 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(routes);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,14 +27,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
 
 db.Workout.create({ name: "Workout" }).then((dbWorkouts) => {
   console.log(dbWorkouts);
-});
-
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/public/stats.html`));
-});
-
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/public/exercise.html`));
 });
 
 app.listen(PORT, () => {
