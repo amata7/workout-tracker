7ff2e8f02b0703ef0a94230b88cda2a46b685df8
amata7 <adrianmata@live.com>
2021-05-07 15:46:56 -0700

    added route to get the workouts from the api

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 const db = require("../models/Workout");
 
+router.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
