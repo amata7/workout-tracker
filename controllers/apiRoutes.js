d380f019d25b52d53b4ed9756f182850a4268d26
amata7 <adrianmata@live.com>
2021-05-08 01:15:41 -0700

    added post route for exercise

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -3,12 +3,23 @@ const db = require("../models/Workout");
 
 router.get("/workouts", (req, res) => {
   db.Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
+    .then((workoutdb) => {
+      res.json(workoutdb);
     })
     .catch((err) => {
       res.json(err);
     });
 });
 
+router.put("/exercise", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((workoutdb) => {
+      res.json(workoutdb);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// how to aggregate fields an array using mongoose
 module.exports = router;
--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const path = require("path");
+const db = require("../models/Workout");
 
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
@@ -9,4 +10,13 @@ router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((workoutdb) => {
+      res.json(workoutdb);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 module.exports = router;
