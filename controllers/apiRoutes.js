26349e98f1eb6ce3b35c2a4ad06f07bdb7426ce1
amata7 <adrianmata@live.com>
2021-05-09 18:28:41 -0700

    user able to create new workouts now. total distance and total duration fields also created

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,24 +1,38 @@
 const router = require("express").Router();
 const db = require("../models/Workout");
 
-router.get("/workouts", (req, res) => {
-  db.Workout.find({})
-    .then((workoutdb) => {
-      res.json(workoutdb);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+// router.get("/workouts", (req, res) => {
+//   db.Workout.find({})
+//     .then((workoutdb) => {
+//       res.json(workoutdb);
+//     })
+//     .catch((err) => {
+//       res.json(err);
+//     });
+// });
+
+router.get("/workouts", async (req, res) => {
+  const workOuts = await db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalWeight: { $sum: "$exercises.weight" },
+      },
+    },
+  ]);
+  res.json(workOuts);
 });
 
-router.post("/exercise", ({ body }, res) => {
-  db.Workout.create(body)
-    .then((workoutdb) => {
-      res.json(workoutdb);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+router.post("/workouts", async (req, res) => {
+  const newWorkout = await db.Workout.create(req.body);
+  res.json(newWorkout);
+});
+
+router.put("/workouts/:id", async (req, res) => {
+  const exercise = await db.Workout.findByIdAndUpdate(req.params.id, {
+    $push: { exercises: req.body },
+  });
+  res.json(exercise);
 });
 
 module.exports = router;
