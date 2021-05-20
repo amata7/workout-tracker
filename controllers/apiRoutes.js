232a0fb66dd92d7ffdbc7abfb12bff9a0aff69ae
amata7 <adrianmata@live.com>
2021-05-19 21:19:01 -0700

    deleted some code

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,16 +1,6 @@
 const router = require("express").Router();
 const db = require("../models/Workout");
 
-// router.get("/workouts", (req, res) => {
-//   db.Workout.find({})
-//     .then((workoutdb) => {
-//       res.json(workoutdb);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
-
 router.get("/workouts", async (req, res) => {
   const workOuts = await db.Workout.aggregate([
     {
